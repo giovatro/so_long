81ea920c4686cba3ba8ae6a484f05dc08bc6b37a
Giovanni Troiano <136205893+giovatro@users.noreply.github.com>
2024-02-09 16:54:42 +1030

    Add files via upload

---
+++ game_logic/initialise.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initialise.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtroiano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 18:16:56 by gtroiano          #+#    #+#             */
+/*   Updated: 2024/02/08 18:17:01 by gtroiano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/so_long.h"
+
+static void	ft_init_help(t_data *data)
+/* only exists because of the 25 line limit */
+{
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+
+	relative_path = "./textures/player_right.xpm";
+	data->img->player_right = mlx_xpm_file_to_image(data->mlx, relative_path,
+			&img_width, &img_height);
+	relative_path = "./textures/background.xpm";
+	data->img->background = mlx_xpm_file_to_image(data->mlx, relative_path,
+			&img_width, &img_height);
+}
+
+void	ft_init(t_data *data, t_map *map)
+/* initialises most of my used data inside the structs */
+{
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+	t_img	*img;
+
+	data->map = map;
+	img = malloc(sizeof(t_img));
+	if (!img)
+	{
+		perror("Error\nmalloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	data->img = img;
+	relative_path = "textures/player_up.xpm";
+	data->img->player_up = mlx_xpm_file_to_image(data->mlx, relative_path,
+			&img_width, &img_height);
+	relative_path = "./textures/player_left.xpm";
+	data->img->player_left = mlx_xpm_file_to_image(data->mlx, relative_path,
+			&img_width, &img_height);
+	relative_path = "./textures/player_down.xpm";
+	data->img->player_down = mlx_xpm_file_to_image(data->mlx, relative_path,
+			&img_width, &img_height);
+	ft_init_help(data);
+	data->counter = 0;
+	data->collected = 0;
+}
---
+++ libft/ft_putptr.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtroiano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 15:19:34 by gtroiano          #+#    #+#             */
+/*   Updated: 2023/08/14 17:23:20 by gtroiano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/libft.h"
+
+void	ft_putptr(void *p, int *len)
+{
+	unsigned long	ptr_address;
+
+	ptr_address = (unsigned long)p;
+	ft_putstr("0x", len);
+	ft_puthex(ptr_address, len, 'x');
+}
